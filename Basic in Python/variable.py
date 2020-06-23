b398ce29cc8c426bae9f00a1b893f2bb72a2f65b
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-06-23 18:54:29 +0530

    Create variable.py

---
+++ Basic in Python/variable.py
@@ -0,0 +1,15 @@
+>>> name = 'youtube'
+>>> name
+'youtube'
+>>> name [1:]
+'outube'
+>>> name [1:4] + ' my'
+'out my'
+>>> name
+'youtube'
+>>> 'my '+ name[1:4]
+'my out'
+>>> myname = 'Naman Kumar'
+>>> len(myname)
+11
+>>> 
