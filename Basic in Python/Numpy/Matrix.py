3f57cd1392cf9c690e21fe2e512bd2824539dc29
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-07-07 21:00:10 +0530

    Matrix

---
+++ Basic in Python/Numpy/Matrix.py
@@ -0,0 +1,23 @@
+from numpy import *
+'''
+arr1 = array ([
+
+                   [1,2,3,6],
+                   [4,5,6,7]
+
+
+            ])
+
+'''
+m = matrix('1 2 3 ; 6 4 5 ; 1 6 7')
+
+'''arr2 = arr1.flatten()
+
+arr3 = arr2.reshape(2,2,3)
+'''
+
+#print((m)
+
+print(diagonal(m))
+
+
