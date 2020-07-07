ac4d8c07265ae41b66b2c95d7319948c217f35ee
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-07-07 17:52:12 +0530

    Create ConcatenationArray.py

---
+++ Basic in Python/Array/ConcatenationArray.py
@@ -0,0 +1,17 @@
+from numpy import *
+
+arr1 = array([1,2,3,4,5])
+
+arr2 = array([6,1,9,3,2])
+
+'''
+arr3 = arr1 + arr2
+
+print(arr3)
+
+print(sin(arr1))
+
+print(max(arr1))
+'''
+
+print(concatenate([arr1,arr2]))
