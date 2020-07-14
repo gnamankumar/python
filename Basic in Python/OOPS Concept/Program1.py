8103cad9048443af599be3ff06378c46ef163e45
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-07-14 22:24:17 +0530

    Class and Object

---
+++ Basic in Python/OOPS Concept/Program1.py
@@ -0,0 +1,25 @@
+
+
+class Computer:
+
+
+    def config(self):
+        print("i7 , 16gb , 1TB")
+'''
+x = 9
+print(type(x))
+
+    
+a = '5'
+print(type(a))
+'''
+
+com1 = Computer()
+com2 = Computer()
+
+Computer.config(com1)
+Computer.config(com2)
+
+com1.config()
+com2.config()
+
