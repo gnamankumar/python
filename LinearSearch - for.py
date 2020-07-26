e049a29fad8c5147535c6f2f26bf60675ebed9b7
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-07-26 19:19:03 +0530

    Using For Loop

---
+++ LinearSearch - for.py
@@ -0,0 +1,24 @@
+pos = -1
+
+
+def search(list, n):
+    i = 0
+
+    for i in range(len(list)):
+        if list[i] == n:
+            globals()['pos'] = i
+            return True
+
+        
+
+    return False
+
+
+list = [5,8,4,6,9,2]
+n = 2
+
+if search(list , n):
+    print("Found at ",pos)
+
+else:
+    print("Not Found")
