c2a50657884a598a70837b4f6687e786dd12bfe9
Naman Kumar <42366641+gnamankumar@users.noreply.github.com>
2020-08-31 23:30:55 +0530

    Create String.py

---
+++ Practice/String.py
@@ -0,0 +1,13 @@
+# characters of String 
+  
+String1 = "Namankumar"
+print("Initial String: ") 
+print(String1) 
+  
+# Printing First character 
+print("\nFirst character of String is: ") 
+print(String1[0]) 
+  
+# Printing Last character 
+print("\nLast character of String is: ") 
+print(String1[-1]) 
